c87c543fcb3b50d12a8ba73eeb7c169c4821e5d0
camisgarciat <camisgarciat@icloud.com>
2024-03-22 09:09:47 -0300

    Comentando o codigo

--- app/src/main/java/nascimento/garcia/primeiraapp/MainActivity.java
+++ app/src/main/java/nascimento/garcia/primeiraapp/MainActivity.java
@@ -13,17 +13,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Constroi a interface a partir da leitura do arquivo de layout
         setContentView(R.layout.activity_main);
+
+        //Conecta o id "btnEnviar" ao botao da interface para obte-lo
         Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        //Define o que acontece quando clica no botao
         btnEnviar.setOnClickListener(new View.OnClickListener(){
             @Override
+            //O OnClick é chamado quando da o clique do botao
             public void onClick(View v) {
+                //Recebe o texto que o usuario digitou (obtem campo de texto)
                 EditText etDigiteAqui= findViewById(R.id.etDigiteAqui);
+                //Transforma em String
                 String textoDigitado = etDigiteAqui.getText().toString();
-                //Cria uma intenção que navega de uma tela para outra
+                //Cria uma intencao que navega de uma tela para outra
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
+                //Coloca o texto dentro da intencao pra enviar para proxima tela
                 i.putExtra("texto", textoDigitado);
-                //inicia a intenção
+                //executa a intencao e navegua para proxima tela
                 startActivity(i);
 
 
--- app/src/main/java/nascimento/garcia/primeiraapp/NextActivity.java
+++ app/src/main/java/nascimento/garcia/primeiraapp/NextActivity.java
@@ -12,10 +12,13 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-
+        //Obtem o intent que criou esssa tela
         Intent i = getIntent();
+        //Obtem o texto da tela anterior
         String texto= i.getStringExtra("texto");
+        //obtem o TextView
         TextView tvTexto = findViewById(R.id.tvTexto);
+        //seta o texto exibido como o texto do usuario (seta o TextView)
         tvTexto.setText(texto);
     }
 }
